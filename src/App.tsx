ddca24e7456156ebf56aea9ed60fd5127f468dc8
hadizha <user@hadizhas-mac.local>
2024-07-02 15:10:24 +0600

    mui installed

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,8 @@ function App() {
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
-      </p>
+        Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more
     </>
   )
 }
